417ec443c3c0be8ba4bf260aac1cce22e8eb0de8
Cesar Roman <cesardroman18@gmail.com>
2020-12-04 17:53:46 -0500

    Finished first three drills

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [data, setData] = useState([
+    'spaghetti',
+    'ice cream',
+    'sushi',
+    'bologna',
+    'cheese',
+  ]);
+
+  const foodsToDisplay = data.map((string, idx) => {
+    return <h2 key={idx}>{string}</h2>;
+  });
+
+  return <div className="App">{foodsToDisplay}</div>;
+};
 
 export default App;
